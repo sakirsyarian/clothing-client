2e1217138c0fd47ec74415d227f6154dafdc2813
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-25 15:55:09 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="border">
+    <NavbarSection />
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import NavbarSection from '@/components/NavbarSection.vue'
+</script>
---
+++ src/components/NavbarSection.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="mx-auto container border">
+        <div class="flex gap-5">
+            <RouterLink class="btn" to="/">Home</RouterLink>
+            <RouterLink class="btn" to="/about">About</RouterLink>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
