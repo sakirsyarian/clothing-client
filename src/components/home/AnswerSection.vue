6e9a2a145ff524a556aaa0654b007ca38b9372fb
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-26 15:55:57 +0700

    feat: Faq

---
+++ src/components/home/AnswerSection.vue
@@ -0,0 +1,69 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="py-10">
+        <!-- heading -->
+        <div class="">
+            <h2 class="py-5 font-semibold text-3xl uppercase">FAQ</h2>
+        </div>
+
+        <!-- answer -->
+        <div class="grid gap-5">
+            <div class="collapse collapse-plus bg-base-100 rounded-none border">
+                <input type="radio" name="my-accordion-3" checked="checked" />
+                <div class="collapse-title text-xl font-medium">
+                    Bagaimana cara berbelanja di Elgoritme?
+                </div>
+                <div class="collapse-content">
+                    <p>
+                        Untuk berbelanja di website fashion store, Anda perlu membuat akun terlebih dahulu dengan mengisi
+                        data diri dan alamat pengiriman. Kemudian, Anda bisa memilih produk yang Anda inginkan dari katalog
+                        yang tersedia. Setelah itu, Anda bisa menambahkan produk ke keranjang belanja dan melanjutkan ke
+                        proses pembayaran. Anda bisa memilih metode pembayaran yang sesuai dengan preferensi Anda, seperti
+                        transfer bank, kartu kredit, atau e-wallet. Setelah pembayaran berhasil, Anda akan mendapatkan
+                        konfirmasi dan nomor resi pengiriman melalui email.
+                    </p>
+                </div>
+            </div>
+            <div class="collapse collapse-plus bg-base-100 rounded-none border">
+                <input type="radio" name="my-accordion-3" />
+                <div class="collapse-title text-xl font-medium">
+                    Apa saja keuntungan berbelanja di Elgoritme?
+                </div>
+                <div class="collapse-content">
+                    <p>Berbelanja di website fashion store memiliki banyak keuntungan, antara lain:</p>
+                    <ul class="mt-2">
+                        <li>- Anda bisa mendapatkan produk fashion terbaru dan berkualitas dari berbagai merek ternama</li>
+                        <li>- Anda bisa menikmati diskon, promo, dan voucher menarik yang ditawarkan oleh website fashion
+                            store</li>
+                        <li>- Anda bisa menghemat waktu dan biaya transportasi karena tidak perlu keluar rumah untuk
+                            berbelanja</li>
+                        <li>- Anda bisa mendapatkan layanan pengiriman cepat dan aman ke seluruh Indonesia</li>
+                        <li>- Anda bisa mendapatkan garansi dan layanan retur jika produk yang Anda terima tidak sesuai
+                            dengan
+                            deskripsi atau cacat
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="collapse collapse-plus bg-base-100 rounded-none border">
+                <input type="radio" name="my-accordion-3" />
+                <div class="collapse-title text-xl font-medium">
+                    Bagaimana cara menghubungi layanan pelanggan Elgoritme?
+                </div>
+                <div class="collapse-content">
+                    <p>Jika Anda memiliki pertanyaan, keluhan, atau saran mengenai website fashion store, Anda bisa
+                        menghubungi layanan pelanggan kami melalui beberapa cara berikut:</p>
+                    <ul class="mt-2">
+                        <li>- Mengirim email ke elgoritme@mail.com kami</li>
+                        <li>- Mengisi formulir contact yang tersedia di website kami</li>
+                        <li>- Menghubungi nomor telepon 021-123456 kami</li>
+                        <li>- Mengirim pesan melalui @elgoritme kami</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,24 @@
 <script setup>
 import NavbarSection from '@/components/NavbarSection.vue';
 import HeroSection from '@/components/home/HeroSection.vue';
+import AnswerSection from '@/components/home/AnswerSection.vue';
 </script>
 
 <template>
-  <header class="bg-base-100">
-    <div class="mx-auto container">
-      <div class="px-4 xl:px-40">
-        <NavbarSection />
-      </div>
-    </div>
-  </header>
+    <header class="bg-base-100">
+        <div class="mx-auto container">
+            <div class="px-4 xl:px-40">
+                <NavbarSection />
+            </div>
+        </div>
+    </header>
 
-  <main class="bg-base-100">
-    <div class="mx-auto container">
-      <div class="px-4 xl:px-40">
-        <HeroSection />
-      </div>
-    </div>
-  </main>
+    <main class="bg-base-100">
+        <div class="mx-auto container">
+            <div class="px-4 xl:px-40">
+                <HeroSection />
+                <AnswerSection />
+            </div>
+        </div>
+    </main>
 </template>
