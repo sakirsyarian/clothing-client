f2d83805e23bbf30328779883cb46554187a15aa
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-26 20:05:59 +0700

    feat: Footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import NavbarSection from '@/components/NavbarSection.vue';
 import HeroSection from '@/components/home/HeroSection.vue';
 import AnswerSection from '@/components/home/AnswerSection.vue';
 import ContactSection from '@/components/home/ContactSection.vue';
+import FooterSection from '@/components/FooterSection.vue';
 </script>
 
 <template>
@@ -23,4 +24,12 @@ import ContactSection from '@/components/home/ContactSection.vue';
             </div>
         </div>
     </main>
+
+    <footer class="bg-black">
+        <div class="mx-auto container">
+            <div class="px-4 xl:px-40">
+                <FooterSection />
+            </div>
+        </div>
+    </footer>
 </template>
