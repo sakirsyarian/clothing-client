9f7a2a6028fd5e7bf69e16853b668e7a2eaec808
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-30 16:55:33 +0700

    feat: Midtrans embedded

--- src/components/shop/CheckoutSection.vue
+++ src/components/shop/CheckoutSection.vue
@@ -1,11 +1,48 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
+import { useRouter, onBeforeRouteLeave } from 'vue-router'
 
+import { post } from '@/lib/axios'
 import rupiah from '@/utils/rupiah'
 import totalPrice from '@/utils/totalPrice'
 import { useProductStore } from '@/stores/product';
 
+const loading = ref(true)
+const router = useRouter()
 const productStore = useProductStore();
+
+onBeforeRouteLeave((to, from) => {
+    console.clear()
+    if (from.name === 'checkout') window.snap.hide()
+})
+
+async function orderNow() {
+    try {
+        const { data: midtrans } = await post('midtrans-token', productStore.shoppingCart)
+        loading.value = false
+
+        window.snap.embed(midtrans.data.token, {
+            embedId: 'snap-container',
+            onSuccess: function (result) {
+                productStore.shoppingCart.length = 0;
+                console.log(result);
+                router.push('/')
+            },
+            onPending: function (result) {
+                alert("wating your payment!"); console.log(result);
+            },
+            onError: function (result) {
+                alert("payment failed!"); console.log(result);
+            },
+            onClose: function () {
+                alert('you closed the popup without finishing the payment');
+            }
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 onMounted(async () => {
     await productStore.getProducts()
 });
@@ -13,15 +50,19 @@ onMounted(async () => {
 
 <template>
     <section class="py-5">
-        <div class="grid grid-cols-1 md:grid-cols-3 justify-between gap-5 border-t">
+        <div class="grid grid-cols-1 md:grid-cols-3 justify-between gap-5 border-t h-full">
             <div class="md:col-span-2">
                 <h2 class="py-5 font-semibold uppercase">Payment</h2>
+                <div v-if="loading" class="py-5 flex items-center justify-center gap-2 bg-gray-50">
+                    <span class="loading loading-spinner loading-md"></span>
+                </div>
+                <div id="snap-container" class="w-full"></div>
             </div>
             <div class="">
                 <h2 class="py-5 font-semibold uppercase">Order Summary</h2>
 
-                <div v-if="!productStore.shoppingCart.length" class="p-5 bg-base-200 border">
-                    <p class="uppercase text-base">Data not available!</p>
+                <div v-if="!productStore.shoppingCart.length" class="p-5 bg-gray-50">
+                    <p class="uppercase text-sm">Data not available!</p>
                 </div>
 
                 <div v-if="productStore.shoppingCart.length" class="p-5 space-y-5 bg-gray-50 border">
@@ -41,7 +82,9 @@ onMounted(async () => {
                         <p class="font-semibold uppercase">Total</p>
                         <p class="font-semibold">{{ totalPrice(productStore.shoppingCart) }}</p>
                     </div>
-                    <button className="btn py-4 w-full rounded-none hover:bg-black bg-black text-base-100">
+
+                    <button @click="orderNow"
+                        className="btn py-4 w-full rounded-none hover:bg-black bg-black text-base-100">
                         Order Now
                     </button>
                 </div>
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,21 @@ export function get(url) {
   return axios.get(url, {
     headers: {
       access_token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkzMjkzNjk2LCJleHAiOjE2OTMzODAwOTZ9.g_ldooItKQAdcVUelf61AhLgMteJ3LfxNBvpR965sjQ'
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkzMzY5NzEwLCJleHAiOjE2OTM0NTYxMTB9.8KpgbqOTRlO-tCAYOzekYlLRbuElgigndorbVXqQq1c'
+    }
+  })
+}
+
+export function post(url, product) {
+  return axios({
+    method: 'post',
+    url: `http://localhost:3000/${url}`,
+    headers: {
+      access_token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkzMzY5NzEwLCJleHAiOjE2OTM0NTYxMTB9.8KpgbqOTRlO-tCAYOzekYlLRbuElgigndorbVXqQq1c'
+    },
+    data: {
+      shoppingCart: product
     }
   })
 }
