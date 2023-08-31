0c296b797572a66765664b6360fb6cfaf7055335
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-31 12:35:50 +0700

    refactor: Catalog page

--- src/components/catalog/ListSection.vue
+++ src/components/catalog/ListSection.vue
@@ -1,20 +1,16 @@
 <script setup>
-import { onMounted } from 'vue'
 import { RouterLink } from 'vue-router'
-
 import { useProductStore } from '@/stores/product';
 
+defineProps(['products']);
 const productStore = useProductStore();
-onMounted(async () => {
-    await productStore.getProducts()
-});
 </script>
 
 <template>
     <section class="md:col-span-3 lg:col-span-4 border-t">
         <!-- tshirt -->
         <div class="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 font-semibold">
-            <div v-for="product in productStore.products" :key="product.id" class="p-5 space-y-5 uppercase bg-base-200">
+            <div v-for="product in products" :key="product.id" class="p-5 space-y-5 uppercase bg-base-200">
                 <RouterLink :to="'/product/' + product.id">{{ product.name }}</RouterLink>
                 <img :src="'/products/' + product.image" class="mx-auto" :alt="product.name">
                 <div class="flex flex-row md:flex-col lg:flex-row justify-between">
