8237c66ede15e39090b6637fdb5e43854b6fb0a0
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-31 10:33:27 +0700

    backup: Related section

--- src/components/product/InformationSection.vue
+++ src/components/product/InformationSection.vue
@@ -2,42 +2,52 @@
 import { ref, onMounted } from 'vue';
 import { useRoute, RouterLink } from 'vue-router'
 
-import { useProductStore } from '@/stores/product';
+// import { useProductStore } from '@/stores/product';
 
-const route = useRoute()
-const quantity = ref(0);
-const productDetail = ref({});
-const productStore = useProductStore();
+const props = defineProps([
+    'product',
+    'quantity',
+    'quantityPlus',
+    'quantityMinus',
+    'addToChart']
+);
+// console.log("information section");
+// console.log(props.product, "from props");
 
-function quantityOnShoppingCart() {
-    const found = productStore.shoppingCart.find((el) => el.id === productDetail.value.id)
-    if (productStore.shoppingCart.length && found) {
-        found.quantity = quantity.value
-    }
-}
+// const route = useRoute()
+// const quantity = ref(0);
+// const productDetail = ref({});
+// const productStore = useProductStore();
 
-function quantityPlus() {
-    quantity.value++
-    productDetail.value.quantity = quantity.value
-    quantityOnShoppingCart()
-}
+// function quantityOnShoppingCart() {
+//     const found = productStore.shoppingCart.find((el) => el.id === productDetail.value.id)
+//     if (productStore.shoppingCart.length && found) {
+//         found.quantity = quantity.value
+//     }
+// }
 
-function quantityMinus() {
-    quantity.value--
-    productDetail.value.quantity = quantity.value
-    quantityOnShoppingCart()
-}
+// function quantityPlus() {
+//     quantity.value++
+//     productDetail.value.quantity = quantity.value
+//     quantityOnShoppingCart()
+// }
 
-function addToChart() {
-    productDetail.value.quantity = quantity.value
-    productStore.purchase(productDetail.value.id)
-}
+// function quantityMinus() {
+//     quantity.value--
+//     productDetail.value.quantity = quantity.value
+//     quantityOnShoppingCart()
+// }
+
+// function addToChart() {
+//     productDetail.value.quantity = quantity.value
+//     productStore.purchase(productDetail.value.id)
+// }
 
 onMounted(async () => {
-    await productStore.getProducts()
+    // await productStore.getProducts()
+    // productDetail.value = productStore.products.find((el) => el.id === +route.params.id)
 
-    productDetail.value = productStore.products.find((el) => el.id === +route.params.id)
-    quantity.value = productDetail.value.quantity
+    // quantity.value = props.product.quantity
 });
 </script>
 
@@ -53,33 +63,33 @@ onMounted(async () => {
                 <li>
                     <RouterLink to="/catalog">Product</RouterLink>
                 </li>
-                <li>{{ productDetail.name }}</li>
+                <li>{{ product.name }}</li>
             </ul>
         </div>
 
         <!-- heading -->
         <div class="space-y-5">
-            <h2 @click="quantityProduct" class="font-semibold text-2xl uppercase">{{ productDetail.name }}</h2>
-            <p class="uppercase text-lg text-gray-600">{{ productDetail.rupiah }}</p>
+            <h2 @click="quantityProduct" class="font-semibold text-2xl uppercase">{{ product.name }}</h2>
+            <p class="uppercase text-lg text-gray-600">{{ product.rupiah }}</p>
         </div>
 
         <!-- info -->
         <div class="space-y-2 text-gray-500">
-            <p>Stock: {{ productDetail.stock }}</p>
-            <p>Category: {{ productDetail.Category?.name }}</p>
+            <p>Stock: {{ product.stock }}</p>
+            <p>Category: {{ product.Category?.name }}</p>
         </div>
 
         <!-- description -->
         <div class="space-y-2">
             <p>Description :</p>
-            <p class="text-gray-500">{{ productDetail.description }} </p>
+            <p class="text-gray-500">{{ product.description }} </p>
         </div>
     </div>
 
     <!-- image -->
     <div class="w-full">
         <div class="p-10 bg-base-200">
-            <img :src="'/products/' + productDetail.image" class="mx-auto h-96" :alt="productDetail.name">
+            <img :src="'/products/' + product.image" class="mx-auto h-96" :alt="product.name">
         </div>
     </div>
 
--- src/components/product/RelatedSection.vue
+++ src/components/product/RelatedSection.vue
@@ -1,5 +1,31 @@
 <script setup>
+import { ref, onMounted, onUpdated } from 'vue'
+import { useProductStore } from '@/stores/product';
 
+const props = defineProps([
+    'products']
+);
+
+const random = ref([])
+const productStore = useProductStore();
+
+function randomProducts(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+onMounted(async () => {
+    await productStore.getProducts()
+    random.value = randomProducts(productStore.products)
+        .slice(0, 5)
+});
+
+onUpdated(async () => {
+    // await productStore.getProducts()
+}) 
 </script>
 
 <template>
@@ -8,41 +34,14 @@
         <h2 class="font-semibold text-2xl uppercase">You May Also Like</h2>
     </div>
 
-    <!-- tshirt -->
-    <div class="py-5 flex flex-col md:flex-row gap-5 font-semibold">
-        <div class="p-5 space-y-5 uppercase bg-base-200">
-            <p class="text-right">JavaScript</p>
-            <img src="/img/javascript.png" class="mx-auto" alt="javascript">
+    <!-- shirt -->
+    <div class="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 font-semibold">
+        <div v-for="product in products" :key="product.id" class="p-5 space-y-5 uppercase bg-base-200">
+            <RouterLink :to="'/product/' + product.id">{{ product.name }}</RouterLink>
+            <img :src="'/products/' + product.image" class="mx-auto" :alt="product.name">
             <div class="flex flex-row md:flex-col lg:flex-row justify-between">
-                <p>Rp 56.000</p>
-                <a href="#" class="underline">Buy</a>
-            </div>
-        </div>
-
-        <div class="p-5 space-y-5 uppercase bg-base-200">
-            <p class="text-right">Vue</p>
-            <img src="/img/vue.png" class="mx-auto" alt="vue">
-            <div class="flex flex-row md:flex-col lg:flex-row justify-between">
-                <p>Rp 55.000</p>
-                <a href="#" class="underline">Buy</a>
-            </div>
-        </div>
-
-        <div class="p-5 space-y-5 uppercase bg-base-200">
-            <p class="text-right">React</p>
-            <img src="/img/react.png" class="mx-auto" alt="react">
-            <div class="flex flex-row md:flex-col lg:flex-row justify-between">
-                <p>Rp 59.000</p>
-                <a href="#" class="underline">Buy</a>
-            </div>
-        </div>
-
-        <div class="p-5 space-y-5 uppercase bg-base-200">
-            <p class="text-right">JavaScript</p>
-            <img src="/img/javascript.png" class="mx-auto" alt="javascript">
-            <div class="flex flex-row md:flex-col lg:flex-row justify-between">
-                <p>Rp 56.000</p>
-                <a href="#" class="underline">Buy</a>
+                <p>{{ product.rupiah }}</p>
+                <button @click="productStore.purchase(product.id)" class="underline">Buy</button>
             </div>
         </div>
     </div>
